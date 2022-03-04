44a71e6fd956b8c0d8fa7e6ceac5310009357c15
Alex Plasencia <aPlasencia895@hotmail.com>
2022-03-04 06:04:08 +0000

    Make all three dog images presentable by using css as well as putting a card container around them.  Added logic where if three pictures already exist, remove those and generate three new pictures.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 var $uList = document.querySelector('.api-dog');
 var $getDog = document.querySelector('.get-dog');
-// console.log('value of $ulist is: ', $uList);
-// console.log('value of $getDog is: ', $getDog);
+var $placeholderImg = document.querySelector('.placeholder-img');
 
 function get3Images() {
+  $placeholderImg.className = 'placeholder-img hidden';
+  var $li = document.querySelectorAll('li');
+  if ($li.length !== 0) {
+    for (var j = 0; j < $li.length; j++) {
+      $li[j].remove();
+    }
+  }
   var xhrImage = new XMLHttpRequest();
   xhrImage.open('GET', 'https://dog.ceo/api/breeds/image/random/3');
   xhrImage.responseType = 'json';
   xhrImage.addEventListener('load', function () {
-    // console.log(xhrImage.status);
-    // console.log(xhrImage.response);
+
     for (var i = 0; i < xhrImage.response.message.length; i++) {
 
       var listedElement = document.createElement('li');
       var imgElement = document.createElement('img');
       imgElement.setAttribute('src', xhrImage.response.message[i]);
+      imgElement.setAttribute('class', 'dog-img');
       listedElement.appendChild(imgElement);
       $uList.appendChild(listedElement);
     }
